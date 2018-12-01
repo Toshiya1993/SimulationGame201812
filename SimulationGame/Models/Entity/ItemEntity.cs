bf9cf8f542c2c48fb9b24e4b736719368fdd7854
Toshiya1993 <1993.tnnt.0807+github@gmail.com>
2018-12-01 20:03:05 +0900

    商品の実態クラス実装

---
+++ SimulationGame/Models/Entity/ItemEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationGame.Models.Entity
+{
+    /// <summary>
+    /// 商品実体クラス
+    /// </summary>
+    class ItemEntity
+    {
+        /// <summary>商品ID</summary>
+        public int Id { get; private set; }
+        /// <summary>値段</summary>
+        public long Price { get; private set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="id">商品ID</param>
+        /// <param name="price">値段</param>
+        public ItemEntity(int id, long price)
+        {
+            this.Id = id;
+            this.Price = price;
+        }
+    }
+}
